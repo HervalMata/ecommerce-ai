9d7adcd50add98e82b441d2c7ca5b3e443243cdd
Herval Mata <hervalmataamparo@gmail.com>
2026-03-04 02:32:16 -0300

    :sparkles: feat: ecommerce ai - create sanity types and cart store

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import {categoryType} from "@/sanity/schemaTypes/categoryType";
+import {productType} from "@/sanity/schemaTypes/productType";
+import {orderType} from "@/sanity/schemaTypes/orderType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [categoryType, productType, orderType],
 }
